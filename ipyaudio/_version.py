ede679af14bad3066cec7e49b88362c75a9370c8
pengzhendong <275331498@qq.com>
2025-04-22 16:11:02 +0800

    Bump to 0.1.18

--- ipyaudio/_version.py
+++ ipyaudio/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Zhendong Peng.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 16)
-__version__ = ".".join(map(str, version_info))
+__version__ = "0.1.18"
+version_info = list(map(int, __version__.split(".")))
